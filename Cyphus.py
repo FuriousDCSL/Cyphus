c50691da9a5701225edd13fa035610ceb0240a74
Unknown <broderick1@gmail.com>
2018-05-13 02:59:59 -0700

    Run with the Cyphus.py now

--- Cyphus.py
+++ Cyphus.py
@@ -1,11 +1,21 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-import myUI.mainWindow
+from mainWindow import Ui_MainWindow
+
+class AppWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 if __name__ == "__main__":
+
+    QtCore.QCoreApplication.setApplicationName("Cyphus")
+    QtCore.QCoreApplication.setApplicationVersion("0.01pa")
+    QtCore.QCoreApplication.setOrganizationName("DCSL")
+    QtCore.QCoreApplication.setOrganizationDomain("http://dcsl.me")
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = myUI.mainWindow.Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    ui = AppWindow()
+    ui.show()
     sys.exit(app.exec_())
--- mainWindow.py
+++ mainWindow.py
@@ -289,13 +289,3 @@ class Ui_MainWindow(object):
         self.actionSettings.setText(_translate("MainWindow", "Settings"))
         self.actionAbout.setText(_translate("MainWindow", "About"))
         self.actionLevel_Info.setText(_translate("MainWindow", "Level Info"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
